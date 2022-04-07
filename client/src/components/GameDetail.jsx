69f861429ddd1b329601c705d071e8b0c22e233e
santiagobojanich <santidgz@hotmail.com>
2022-04-07 09:05:14 -0300

    cssDetail

--- client/src/components/GameDetail.jsx
+++ client/src/components/GameDetail.jsx
@@ -1,40 +1,46 @@
-
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { NavLink, useParams } from "react-router-dom";
-import { useEffect } from 'react'
+import { useEffect } from "react";
 import { getvideogame } from "../actions";
-  
+import detail from "./detail.module.css";
 
-export default  function Detail() {
-    const videogame = useSelector(state => state.videogame)
-    const dispatch = useDispatch()
-    let { id } = useParams()
-  
+export default function Detail() {
+  const videogame = useSelector((state) => state.videogame);
+  const dispatch = useDispatch();
+  let { id } = useParams();
 
   useEffect(() => {
-        dispatch(getvideogame(id))
-    }, [dispatch, id,videogame])
+    dispatch(getvideogame(id));
+  }, [dispatch, id]);
 
-    
-    return (
-       <div>
-    
-       <div>
-           <NavLink to='/home'> BACK </NavLink>
-       </div>
+  return (
+    <div className={detail.background}>
+      
+      <div className={detail.link}>
+        <NavLink to="/home"> BACK </NavLink>
+      </div> 
 
-       <div>
-            <h1> {videogame.name} </h1>
-            <p> {videogame.description} </p>
-            <span>{videogame.rating} </span>
-            <ul> {videogame.platforms.map(plat => {
-                return (
-                    <li key={plat}> {plat} </li>
-                )
-            })}</ul>
-            <p>Genres: || {videogame.genres.map(gen=> `${gen} || ` )}</p>
+      <div className={detail.back}>
+        <div >
+        <h1 className={detail.data}> {videogame.name} </h1>
+        
+        <p className={detail.text}> {videogame.description} </p>
+        
+        <span className={detail.rating}>Rating: {videogame.rating} </span>
+        
+        <ul className={detail.genres}>
+          {videogame.platforms &&
+            videogame.platforms.map((plat) => {
+                return <li key={plat}> {plat} </li>;
+            })}
+        </ul>
+        <p className={detail.genres}>
+          Genres: {"  "}
+          {videogame.genres && videogame.genres.map((gen) => ` ${gen} ||`)}
+        </p>
         </div>
       </div>
-    )
+    </div>
+  );
 }
